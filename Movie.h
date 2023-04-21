9399db4299e8da243347ffaf571424c166d1a39b
Timothy Okolowicz <92887299+tokolowicz@users.noreply.github.com>
2023-04-20 20:28:59 -0400

    Added an avgRating variable

--- Movie.h
+++ Movie.h
@@ -12,6 +12,8 @@ class Movie{
         string name;
         int ID;
         vector<double> ratings;
+        double totalRatings; //holds the total sum of the ratings vector
+        double avgRating; //holds the average of the ratings vector
         vector<string> genres;
     public:
         Movie(string name, int ID, vector<string> genres){ //constructor
@@ -19,6 +21,8 @@ class Movie{
             this->ID = ID;
             this->genres = genres;
             this->ratings; // I think this may initialize the vector so we can insert stuff without initialization?
+            this->avgRating = 0;
+            this->totalRatings = 0;
         }
         //getter methods for all of the variables
         string getName(){
@@ -36,5 +40,7 @@ class Movie{
         //setter methods for all of the variables needed
         void setRatings(double rating){
             this->ratings.push_back(rating);
+            totalRatings += rating;
+            avgRating = totalRatings / ratings.size();
         }
 };
