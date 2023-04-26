78b2a6b417868ef450a38b6fef01d608c43c7f61
Timothy Okolowicz <92887299+tokolowicz@users.noreply.github.com>
2023-04-26 15:28:18 -0400

    Fixed Default Constructor

--- Movie.h
+++ Movie.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <algorithm>
 using namespace std;
+#pragma once
 
 class Movie{
     //private variables of the class
@@ -33,56 +34,3 @@ class Movie{
 
         
 };
-Movie::Movie(){
-    this->name = "Fake Movie";
-    this->ID = 9999999;
-    this->avgRating = 0;
-    this->totalRatings = 0;
-    this->genres = {"Swag", "Bimbo", "Suckotash"};
-    this->ratings;
-}
-Movie::Movie(string name, int ID, vector<string> genres){ //constructor
-    this->name = name;
-    this->ID = ID;
-    this->genres = genres;
-    this->ratings; // I think this may initialize the vector so we can insert stuff without initialization?
-    this->avgRating = 0;
-    this->totalRatings = 0;
-}
-//getter methods for all of the variables
-string Movie::getName(){
-    return this->name;
-}
-int Movie::getID(){
-    return this->ID;
-}
-vector<string> Movie::getGenres(){
-    return this->genres;
-}
-vector<double> Movie::getRatings(){
-    return this->ratings;
-}
-double Movie::getAvgRating(){
-    return this->avgRating;
-}
-//setter methods for all of the variables needed
-void Movie::setRatings(double rating){
-    this->ratings.push_back(rating);
-    this->totalRatings += rating;
-    this->avgRating = totalRatings / this->ratings.size();
-}
-void Movie::setID(int ID){
-    this->ID = ID;
-}
-void Movie::setName(string name){
-    this->name = name;
-}
-void Movie::setGenre(string genre){
-    this->genres.push_back(genre);
-}
-void Movie::setIMDB(int ID){
-    this->IMDB = ID;
-}
-int Movie::getIMDB(){
-    return this->IMDB;
-}
